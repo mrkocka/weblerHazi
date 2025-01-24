f8f121d203e09075e04bb2a251615e0ecfae21a5
mrkocka <radics.pepe@gmail.com>
2025-01-24 10:04:00 +0100

    mai0124

--- html/monitor.sh
+++ html/monitor.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 while true; do
-  ps -eo pid,comm,%cpu,%mem --sort=-%cpu | head -n 16 | awk 'NR>1 {print "{\"pid\":" $1 ",\"name\":\"" $2 "\",\"cpu\":" $3 ",\"memory\":" $4 "},"}' | sed '$ s/,$//' > ~/docker_volume/processes.json
+  ps -eo pid,comm,%cpu,%mem --sort=-%cpu | head -n 15 > ~/docker_volume/processes.txt
   sleep 15
 done
+
